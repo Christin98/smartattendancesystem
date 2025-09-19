4cc90b50437f73827fe0b8d682c1ff4cae425300
Christin Koshy <christinkoshy1998@gmail.com>
2025-09-19 23:34:00 +0530

    Fixed Login issues

---
+++ app/src/main/java/com/mandelbulb/smartattendancesystem/ui/components/AppIcons.kt
@@ -0,0 +1,239 @@
+package com.mandelbulb.smartattendancesystem.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.*
+import androidx.compose.material.icons.automirrored.outlined.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material.icons.sharp.*
+import androidx.compose.material.icons.twotone.*
+
+/**
+ * Centralized icon definitions using Material Icons Extended
+ * This provides a much richer set of icons than the default Material Icons
+ */
+object AppIcons {
+    // Authentication & User Icons
+    val UserCircle = Icons.Filled.AccountCircle
+    val Fingerprint = Icons.Filled.Fingerprint
+    val IdCard = Icons.Filled.Badge
+    val UserCheck = Icons.Filled.VerifiedUser
+    val UserPlus = Icons.Filled.PersonAdd
+    val SignIn = Icons.AutoMirrored.Filled.Login
+    val SignOut = Icons.AutoMirrored.Filled.Logout
+    val Lock = Icons.Filled.Lock
+    val Unlock = Icons.Filled.LockOpen
+    val Key = Icons.Filled.Key
+    val Security = Icons.Filled.Security
+    val Shield = Icons.Filled.Shield
+    val AdminPanel = Icons.Filled.AdminPanelSettings
+
+    // Face & Camera Icons
+    val Camera = Icons.Filled.CameraAlt
+    val CameraFront = Icons.Filled.CameraFront
+    val Video = Icons.Filled.Videocam
+    val FaceRecognition = Icons.Filled.FaceRetouchingNatural
+    val Face = Icons.Filled.Face
+    val Face2 = Icons.Filled.Face2
+    val Face3 = Icons.Filled.Face3
+    val Face6 = Icons.Filled.Face6
+    val Eye = Icons.Filled.Visibility
+    val EyeOff = Icons.Filled.VisibilityOff
+    val Scanner = Icons.Filled.Scanner
+    val QrCode = Icons.Filled.QrCode
+    val QrCodeScanner = Icons.Filled.QrCodeScanner
+
+    // Navigation Icons
+    val ArrowBack = Icons.AutoMirrored.Filled.ArrowBack
+    val ArrowForward = Icons.AutoMirrored.Filled.ArrowForward
+    val ArrowUp = Icons.Filled.KeyboardArrowUp
+    val ArrowDown = Icons.Filled.KeyboardArrowDown
+    val ChevronLeft = Icons.Filled.ChevronLeft
+    val ChevronRight = Icons.Filled.ChevronRight
+    val Home = Icons.Filled.Home
+    val Back = Icons.AutoMirrored.Filled.ArrowBackIos
+    val Forward = Icons.AutoMirrored.Filled.ArrowForwardIos
+    val ExitToApp = Icons.AutoMirrored.Filled.ExitToApp
+    val Menu = Icons.Filled.Menu
+    val MoreVert = Icons.Filled.MoreVert
+    val MoreHoriz = Icons.Filled.MoreHoriz
+
+    // Settings & Configuration Icons
+    val Settings = Icons.Filled.Settings
+    val SettingsApplications = Icons.Filled.SettingsApplications
+    val Tune = Icons.Filled.Tune
+    val Build = Icons.Filled.Build
+    val Engineering = Icons.Filled.Engineering
+    val ManageAccounts = Icons.Filled.ManageAccounts
+    val Api = Icons.Filled.Api
+    val DeveloperMode = Icons.Filled.DeveloperMode
+    val Code = Icons.Filled.Code
+    val Terminal = Icons.Filled.Terminal
+    val DataObject = Icons.Filled.DataObject
+
+    // Database & Storage Icons
+    val Database = Icons.Filled.Storage
+    val CloudUpload = Icons.Filled.CloudUpload
+    val CloudDownload = Icons.Filled.CloudDownload
+    val CloudSync = Icons.Filled.CloudSync
+    val CloudOff = Icons.Filled.CloudOff
+    val CloudDone = Icons.Filled.CloudDone
+    val Backup = Icons.Filled.Backup
+    val FolderOpen = Icons.Filled.FolderOpen
+    val SaveAlt = Icons.Filled.SaveAlt
+
+    // Time & Attendance Icons
+    val Clock = Icons.Filled.AccessTime
+    val Alarm = Icons.Filled.AccessAlarm
+    val Calendar = Icons.Filled.CalendarMonth
+    val CalendarToday = Icons.Filled.CalendarToday
+    val DateRange = Icons.Filled.DateRange
+    val Schedule = Icons.Filled.Schedule
+    val Timer = Icons.Filled.Timer
+    val History = Icons.Filled.History
+    val Update = Icons.Filled.Update
+    val EventAvailable = Icons.Filled.EventAvailable
+    val EventBusy = Icons.Filled.EventBusy
+    val WorkHistory = Icons.Filled.WorkHistory
+
+    // Status & Feedback Icons
+    val Check = Icons.Filled.Check
+    val CheckCircle = Icons.Filled.CheckCircle
+    val CheckCircleOutline = Icons.Filled.CheckCircleOutline
+    val Close = Icons.Filled.Close
+    val Cancel = Icons.Filled.Cancel
+    val Error = Icons.Filled.Error
+    val ErrorOutline = Icons.Filled.ErrorOutline
+    val Warning = Icons.Filled.Warning
+    val Info = Icons.Filled.Info
+    val Help = Icons.Filled.Help
+    val HelpOutline = Icons.Filled.HelpOutline
+    val ReportProblem = Icons.Filled.ReportProblem
+    val Dangerous = Icons.Filled.Dangerous
+
+    // Sync & Data Transfer Icons
+    val Sync = Icons.Filled.Sync
+    val SyncAlt = Icons.Filled.SyncAlt
+    val SyncProblem = Icons.Filled.SyncProblem
+    val SyncDisabled = Icons.Filled.SyncDisabled
+    val Refresh = Icons.Filled.Refresh
+    val Autorenew = Icons.Filled.Autorenew
+    val Upload = Icons.Filled.Upload
+    val Download = Icons.Filled.Download
+    val ImportExport = Icons.Filled.ImportExport
+    val SwapVert = Icons.Filled.SwapVert
+    val SwapHoriz = Icons.Filled.SwapHoriz
+    val CompareArrows = Icons.Filled.CompareArrows
+
+    // Location Icons
+    val Location = Icons.Filled.LocationOn
+    val LocationOff = Icons.Filled.LocationOff
+    val LocationSearching = Icons.Filled.LocationSearching
+    val MyLocation = Icons.Filled.MyLocation
+    val Map = Icons.Filled.Map
+    val NearMe = Icons.Filled.NearMe
+    val Navigation = Icons.Filled.Navigation
+    val Explore = Icons.Filled.Explore
+    val Place = Icons.Filled.Place
+
+    // Analytics & Charts Icons
+    val Analytics = Icons.Filled.Analytics
+    val BarChart = Icons.Filled.BarChart
+    val ShowChart = Icons.Filled.ShowChart
+    val TrendingUp = Icons.Filled.TrendingUp
+    val TrendingDown = Icons.Filled.TrendingDown
+    val PieChart = Icons.Filled.PieChart
+    val DonutLarge = Icons.Filled.DonutLarge
+    val Assessment = Icons.Filled.Assessment
+    val Poll = Icons.Filled.Poll
+    val QueryStats = Icons.Filled.QueryStats
+
+    // Company & Work Icons
+    val Business = Icons.Filled.Business
+    val Work = Icons.Filled.Work
+    val WorkOutline = Icons.Filled.WorkOutline
+    val Groups = Icons.Filled.Groups
+    val Group = Icons.Filled.Group
+    val Person = Icons.Filled.Person
+    val PersonOutline = Icons.Filled.PersonOutline
+    val People = Icons.Filled.People
+    val PeopleOutline = Icons.Filled.PeopleOutline
+    val Badge2 = Icons.Filled.Badge
+    val CardMembership = Icons.Filled.CardMembership
+    val ContactMail = Icons.Filled.ContactMail
+
+    // Action Icons
+    val Add = Icons.Filled.Add
+    val Remove = Icons.Filled.Remove
+    val Edit = Icons.Filled.Edit
+    val Delete = Icons.Filled.Delete
+    val DeleteForever = Icons.Filled.DeleteForever
+    val Search = Icons.Filled.Search
+    val FilterList = Icons.Filled.FilterList
+    val Sort = Icons.Filled.Sort
+    val Share = Icons.Filled.Share
+    val Send = Icons.AutoMirrored.Filled.Send
+    val Print = Icons.Filled.Print
+    val Save = Icons.Filled.Save
+
+    // File & Document Icons
+    val InsertDriveFile = Icons.Filled.InsertDriveFile
+    val Description = Icons.Filled.Description
+    val Article = Icons.Filled.Article
+    val FileCopy = Icons.Filled.FileCopy
+    val FileDownload = Icons.Filled.FileDownload
+    val FileUpload = Icons.Filled.FileUpload
+    val AttachFile = Icons.Filled.AttachFile
+    val CloudUploadFile = Icons.Filled.UploadFile
+    val FilePresent = Icons.Filled.FilePresent
+
+    // Communication Icons
+    val Notifications = Icons.Filled.Notifications
+    val NotificationsActive = Icons.Filled.NotificationsActive
+    val NotificationsOff = Icons.Filled.NotificationsOff
+    val Email = Icons.Filled.Email
+    val Message = Icons.Filled.Message
+    val Chat = Icons.Filled.Chat
+    val Forum = Icons.Filled.Forum
+    val Announcement = Icons.Filled.Announcement
+    val Campaign = Icons.Filled.Campaign
+
+    // Device & System Icons
+    val PhoneAndroid = Icons.Filled.PhoneAndroid
+    val Computer = Icons.Filled.Computer
+    val DesktopWindows = Icons.Filled.DesktopWindows
+    val Tablet = Icons.Filled.Tablet
+    val Watch = Icons.Filled.Watch
+    val Memory = Icons.Filled.Memory
+    val Wifi = Icons.Filled.Wifi
+    val WifiOff = Icons.Filled.WifiOff
+    val Bluetooth = Icons.Filled.Bluetooth
+    val Battery = Icons.Filled.BatteryFull
+    val BatteryAlert = Icons.Filled.BatteryAlert
+    val PowerSettings = Icons.Filled.PowerSettingsNew
+
+    // UI & Display Icons
+    val LightMode = Icons.Filled.LightMode
+    val DarkMode = Icons.Filled.DarkMode
+    val Brightness = Icons.Filled.BrightnessHigh
+    val Palette = Icons.Filled.Palette
+    val ColorLens = Icons.Filled.ColorLens
+    val FormatPaint = Icons.Filled.FormatPaint
+    val Wallpaper = Icons.Filled.Wallpaper
+
+    // Miscellaneous Icons
+    val Favorite = Icons.Filled.Favorite
+    val FavoriteBorder = Icons.Filled.FavoriteBorder
+    val Star = Icons.Filled.Star
+    val StarBorder = Icons.Filled.StarBorder
+    val Grade = Icons.Filled.Grade
+    val ThumbUp = Icons.Filled.ThumbUp
+    val ThumbDown = Icons.Filled.ThumbDown
+    val EmojiEmotions = Icons.Filled.EmojiEmotions
+    val SentimentSatisfied = Icons.Filled.SentimentSatisfied
+    val Mood = Icons.Filled.Mood
+    val MoodBad = Icons.Filled.MoodBad
+    val Psychology = Icons.Filled.Psychology
+    val Lightbulb = Icons.Filled.Lightbulb
+}
